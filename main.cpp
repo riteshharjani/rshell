c3d9f14363bcaf132e8d9fbffac0ec26972ba4d2
Ritesh Harjani <ritesh.harjani@gmail.com>
2018-08-12 22:40:45 +0530

    rshell: Add execl or system command functionality
    With this patch now this implementation actually implements
    a unix type shell
    
    Signed-off-by: Ritesh Harjani <ritesh.harjani@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <stdlib.h>
 #include <string>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "lib/linenoise.h"
 
 using namespace std;
@@ -8,7 +11,21 @@ class MyShell {
 public:
     void handle_command(const string &line)
     {
+	int status = 0;
+	int options = 0;
 	cout << "input command: " << line << endl;
+	pid_t pid;
+	pid = fork();
+	if (pid == 0) {
+	    execl("/bin/sh", "sh", "-c", line.c_str(), NULL);
+	} else {
+	    waitpid(pid, &status, options);
+	}
+	/*
+	 * Or you can directly call system command which will
+	 * do same as above. Check man system
+	 */
+	//system(line.c_str());
     }
 
     void run()
